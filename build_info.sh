4d51ae5008439c97c08040b319f73bcb11fd899e
qyw <i.kyb@ya.ru>
2016-08-09 18:37:21 +0300

    Теперь времменный файл создаётся в ОЗУ. Надеюсь..

--- build_info.sh
+++ build_info.sh
@@ -3,10 +3,10 @@
 ## Создаёт или редактирует файл build_info.h
 ## Переписывает файл только если что-то изменилось, благодаря чему модули C, включающие build_info.h не будут перекопилированы.
 
-
-filepath="$(git rev-parse --show-toplevel)/src/build_info.h"
-#filepath=${filepath////\\}  ## Replace all / to \
-temppath="${filepath}~.tmp"
+gitRepoPath="$(git rev-parse --show-toplevel)"
+filepath="$gitRepoPath/src/build_info.h"
+#temppath="${filepath}~.tmp"
+temppath="$(mktemp -p /dev/shm/)"  ## Файл по идее в ОЗУ http://unix.stackexchange.com/a/188537/156608
 
 ## Header
 echo -e "/**\n * This file was created automatically by script build_info.sh.\n * DO NOT EDIT! \n */\n" > $temppath
