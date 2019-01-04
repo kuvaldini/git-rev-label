25fd98c36ef462a067568f5939ab584ea9651d4c
kyb <i.kyb@ya.ru>
2019-01-04 21:37:42 +0000

    Update git-revision.sh

--- git-revision.sh
+++ git-revision.sh
@@ -3,6 +3,12 @@
 ## Repo https://gitlab.com/kyb/build-info-header
 ## Update with:
 ##   wget 'https://gitlab.com/kyb/build-info-header/raw/master/git-revision.sh?inline=false' -qO git-revision.sh  &&  chmod +x git-revision.sh
+## To make this command work as git subcommand `git revision` create link to this script in PATH:
+##   ln -s $PWD/git-revision.sh /usr/local/bin/git-revision
+## Then use it
+##   git revision
+## or
+##   git revision '$refname-c$count-g$short$_dirty'
 
 set -e
 
@@ -14,8 +20,10 @@ short=$($GIT rev-parse --short HEAD)
 long=$($GIT rev-parse HEAD)  #git -C $GitRepo show-ref -h HEAD
 count=$($GIT rev-list --count --first-parent ${BuildInfo_RevName:=HEAD})
 
-dirty=`$GIT diff --quiet || echo DIRTY`  # $GIT diff --quiet || dirty="DIRTY"
+dirty=`$GIT diff --quiet || echo DIRTY`  # $GIT diff --quiet || dirty="dirty"
 _dirty=${dirty:+-$dirty}  # Expands to nothing when $dirty is empty or undefined, and prepends '-' else.
+DIRTY=$( echo $dirty | tr a-z A-Z )
+_DIRTY=$( echo $_dirty | tr a-z A-Z )
 
 tag=$($GIT tag --list --points-at HEAD)
 tag_=${tag:+$tag$_dirty}
@@ -28,7 +36,7 @@ fi
 branch_=$branch$_dirty   # ${branch:+$branch$_dirty}
 
 refname=${tag+$branch}
-format=${1:-'$refname-c$count-g$short$_dirty'}
+format=${1:-'$refname-c$count-g$short$_DIRTY'}
 revision=$( echo $(eval echo "$format") )   #"$refname-c$count-g$short$_dirty"
 
 echo "$revision"
