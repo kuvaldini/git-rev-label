e7887e78c67ca45ab1490e82c821bebe12cb7c9c
kyb <i.kyb@ya.ru>
2019-08-16 23:45:32 +0300

    git subrepo pull homebrew
    subrepo:
      subdir:   "homebrew"
      merged:   "f7bba1f"
    upstream:
      origin:   "https://github.com/ivakyb/homebrew-git-rev-label.git"
      branch:   "master"
      commit:   "a212f99"
    git-subrepo:
      version:  "0.4.0"
      origin:   "???"
      commit:   "???"

--- homebrew/git-rev-label.rb
+++ homebrew/git-rev-label.rb
@@ -3,9 +3,9 @@ require 'formula'
 class GitRevLabel < Formula
 	desc 'Gives information about Git repository revision in format like \'master-c73-gbbb6bec\'. Can fill template string or file with env vars and information from Git. Useful to provide information about version of the program: branch, tag, commit hash, commits count, dirty status.'
 	homepage 'https://gitlab.com/kyb/git-rev-label'
-	url 'https://gitlab.com/kyb/git-rev-label/raw/artifacts/master/git-rev-label'
-	sha256 '2b8789678299250f0b7029ab6cae8055a35e2786f5ce868162e8fcd0cca28bf7'
-	version 'latest'
+	url 'https://gitlab.com/kyb/git-rev-label/raw/artifacts/homebrew/git-rev-label'
+	sha256 '8f801edfda976460bd3b25b31b53bf38227176765ec1680657bcf9195b5e44b4'
+	version '1.118.102'
 	
 	#depends_on "git" => :recommended  ## Usually installed with developer tools.
 
