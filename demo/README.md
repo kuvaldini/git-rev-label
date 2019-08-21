ff1dc9949a7dedd02220462db8c5452484ade5e7
kyb <i.kyb@ya.ru>
2019-08-21 21:49:45 +0300

    Move README contents to demo-script.

--- demo/README.md
+++ demo/README.md
@@ -1,41 +1,5 @@
-```
-#!/usr/bin/env fish
+## [demo-script.fish](demo-script.fish)
 
-git rev-label --version
-git-rev-label --help
-
-git rev-label
-git rev-label '$refname-c\$count-g\$short\$_dirty'
-
-## Fill template header file to pass rev-label to C/C++
-git rev-label --format-from=build_info.template.h | lolcat -F 0.03
-cat build_info.h
-colordiff build_info.template.h build_info.h 
-
-## List variables filled by script, use them in --format
-git rev-label --variables
-
-## Fill a template with variables from environment
-env A="asdf cxz" git rev-label 'hello-$A-$refname'
-
-## Walk over git commits and show how rev-label detects branch, tag, detached, dirty
-echo >a
-git status
-## DIRTY
-git rev-label
-git checkout master^
-git rev-label
-## 133-1=132
-git checkout --detach
-git rev-label
-## DETACHED and DIRTY
-git commit a -m"add a"
-git status
-## no dirty status
-git tag asdf
-git rev-label
-## $tag is used because no branch has been found
-
-git rev-label --update
-
-```
+![](demo-part1.gif)  
+![](demo-part2.gif)  
+![](demo-part3.gif)  
---
+++ demo/demo-script.fish
@@ -0,0 +1,51 @@
+#!/usr/bin/env fish
+
+## PART 1
+## Basic features
+git rev-label --version
+git-rev-label --help
+
+git rev-label
+git rev-label '$refname-c\$count-g\$short\$_dirty'
+git rev-label --format='$branch-C\$COUNT-g\$LONG\$_DIRTY'
+
+
+## PART 2
+## Walk over git commits and show how rev-label detects branch, tag, detached and dirty states
+git status
+## clean
+echo a >a
+git status
+## dirty
+git rev-label
+git checkout HEAD^
+git rev-label
+## count -1, DETACHED and DIRTY
+git commit a -m"add a"
+git status
+git tag brantozyabra
+git rev-label
+## count +1, no dirty status
+## $tag is used because of detached state
+
+
+## PART 3
+## List variables provided by script
+git rev-label --variables
+## Use them in --format string
+
+## Also can take variables from environment
+env A="lovely World" B="git-rev-label" git rev-label 'Hello, my $A, from $B $refname. Here is $unset variable.'
+
+## Fill template header file to pass rev-label to C/C++
+cd build_info-header
+git rev-label --format-from=build_info.template.h | tee build_info.h | lolcat -F 0.03
+colordiff build_info{.template,}.h
+## Variable placeholders were filled
+cd -
+
+
+## PART 4
+## Self-Maintenance
+#todo curl -fsSL https://git-rev-label.sh | bash
+git rev-label --update
