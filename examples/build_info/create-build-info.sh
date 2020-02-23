68c0cf0118b7e8eaf762db0ee82f1360eeb1f84c
kyb <i.kyb@ya.ru>
2020-02-23 10:09:38 +0200

    simplify create-build-info.sh

--- examples/build_info/create-build-info.sh
+++ examples/build_info/create-build-info.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+## Write to file only if there are changes
 ## Example usage: 
 ##    ./create-build-info.sh build_info.h --format="`cat build_info.template.h`"
 ## or
@@ -8,20 +9,10 @@ set -euo pipefail
 
 TargetFile=$1; shift
 
-D=$( dirname $( readlink -f "${BASH_SOURCE[0]}" ))  ## which was removed because of on our windows CI 'which' prints nothing if did not find in arg in PATH 
-
-source "$D/git-rev-label.sh" "$@"
+D=$( dirname $( readlink -f "${BASH_SOURCE[0]}" ))
 
 temppath=`mktemp`
-echo "$revision" >$temppath
-
-## Write to file only if there are changes
-if diff --brief "$temppath" "$TargetFile" &>/dev/null  ;then
-   echo >/dev/stderr "Nothing to change"
-else
-   cp "$temppath" "$TargetFile"  ||  {
-      ret=$0
-      echo Failed writing to "$TargetFile" >/dev/stderr
-      exit $ret
-   }
-fi
+"$D/git-rev-label.sh" "$@" >$temppath
+diff --brief "$temppath" "$TargetFile" &>/dev/null  \
+   && echo >/dev/stderr "$TargetFile is up to date"
+   || cp "$temppath" "$TargetFile"
